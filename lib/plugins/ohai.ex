fd662230fa1d50bf06903daf47240a8613aa1130
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2016-09-10 13:34:59 +0200

    add tests and pimp ohai a bit

--- lib/plugins/ohai.ex
+++ lib/plugins/ohai.ex
@@ -10,15 +10,36 @@ defmodule ELI.Ohai do
     {:ok, client}
   end
 
-  def handle_info({:received, msg, nick, channel}, client) do
-    Logger.info "received -> #{msg}"
-
-    if String.contains?(msg, "hi") do
-      ExIrc.Client.msg client, :privmsg, channel, "hi"
+  def handle_info({:received, msg, info, channel}, client) do
+    if test(msg) do
+      %{ nick: nick } = info
+      ExIrc.Client.msg client, :privmsg, channel, reply(nick)
     end
 
     {:noreply, client}
   end
 
   def handle_info(_msg, client), do: {:noreply, client}
+
+  @doc """
+    ## Examples
+      iex> ELI.Ohai.test "Hi Eli"
+      true
+      iex> ELI.Ohai.test "foo Eli"
+      false
+  """
+  def test(msg) do
+    case String.downcase msg do
+      "hi eli" -> true
+      "hello eli" -> true
+      _ -> false
+    end
+  end
+
+  @doc """
+    ## Examples
+      iex> ELI.Ohai.reply "foo"
+      "hello foo"
+  """
+  def reply(nick), do: "hello #{nick}"
 end
---
+++ test/plugins/ohai_test.exs
@@ -0,0 +1,4 @@
+defmodule ELITest.Ohai do
+  use ExUnit.Case
+  doctest ELI.Ohai
+end
